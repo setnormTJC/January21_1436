21c8f1d2236f9f39b042ba61f1c3a796ca2680a6
Work <Work@MSI>
2025-01-21 10:27:24 -0600

    Add project files.

---
+++ January21_1436/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include"iostream" //what is this!?
+using std::cout; //standard
+int main()
+{
+	cout << "Hello, world."//a semicolon is analogous to a PERIOD in English
+	//It "ends" the sentence/instruction
+	//
+	// What is this green stuff I am typing?
+	//
+	//less than less than
+	return 0;
+}
